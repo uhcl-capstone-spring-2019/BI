4d9a60f11c9e4459a35d262bc09a6ee82916304f
ChandraMouliBasava <38330496+ChandraMouliBasava@users.noreply.github.com>
2019-03-02 12:57:40 -0600

    Add files via upload

---
+++ BusinessLayer/Test/functioncall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Test
+{
+   public class functionCall
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the location: ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the latitude: ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the longitude: ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the buildingType: ");
+            Console.ReadLine();
+        }
+    }
+}
